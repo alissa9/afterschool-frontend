a867b54603a53991661f941a71cf027dd7d88cab
Ikbal Alissa <m99alissa@gmail.com>
2021-11-09 16:31:18 +0000

    added pictures to lessons and search bar

--- Labs work/products.js
+++ Labs work/products.js
@@ -5,7 +5,7 @@ let products = [
         "name": "Mathmatics",
         "location": "Hendon",
         "price": 40.00,
-        "image": "mathsPic.jpg",
+        "image": "images/maths.jpg",
         "spaces": 10,
         "rating": 4
     },
@@ -13,13 +13,93 @@ let products = [
     {
 
         "id": 1002,
-        "name": "Science",
+        "name": "Chemistry",
         "location": "Hendon",
         "price": 35.00,
-        "image": "mathsPic.jpg",
-        "spaces": 8,
+        "image": "images/chemistry.jpg",
+        "spaces": 6,
         "rating": 3
+    },
+
+
+
+    { 
+
+         "id": 1003,
+         "name": "Arabic",
+         "location": "Hendon",
+         "price": 47.00,
+         "image": "images/arabic.jpg",
+         "spaces": 6,
+         "rating": 3
+    },
+
+    {
+        "id": 1004,
+        "name": "IT",
+        "location": "Hendon",
+        "price": 47.00,
+        "image": "images/IT.jpg",
+        "spaces": 6,
+        "rating": 3
+
+    },
+    {
+        "id": 1005,
+        "name": "English",
+        "location": "Hendon",
+        "price": 47.00,
+        "image": "images/english.jpg",
+        "spaces": 6,
+        "rating": 3
+
+    },
+    {
+        "id": 1006,
+        "name": "Drama",
+        "location": "Hendon",
+        "price": 39.00,
+        "image": "images/drama.jpg",
+        "spaces": 5,
+        "rating": 4
+    },
+    {
+        "id": 1007,
+        "name": "Swimming",
+        "location": "Hendon",
+        "price": 55.00,
+        "image": "images/swimming.jpg",
+        "spaces": 5,
+        "rating": 4
+    },
+    {
+        "id": 1008,
+        "name": "History",
+        "location": "Hendon",
+        "price": 46.00,
+        "image": "images/history.jpg",
+        "spaces": 7,
+        "rating": 4
+
+    },
+    {
+        "id": 1009,
+        "name": "Geography",
+        "location": "Hendon",
+        "price": 46.00,
+        "image": "images/geography.jpg",
+        "spaces": 9,
+        "rating": 4
+
+    },
+    {
+        "id": 1010,
+        "name": "Music",
+        "location": "Hendon",
+        "price": 46.00,
+        "image": "images/music.jpg",
+        "spaces": 8,
+        "rating": 4
+
     }
-
-
 ]
