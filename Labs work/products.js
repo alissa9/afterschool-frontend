9bd551dffb4e5148d6c4a08dae7fec2ecf2c72ea
Ikbal Alissa <M99alissa@gmail.com>
2021-11-09 13:47:36 +0000

    updated products

---
+++ Labs work/products.js
@@ -0,0 +1,25 @@
+let products = [
+    {
+
+        "id": 1001,
+        "name": "Mathmatics",
+        "location": "Hendon",
+        "price": 40.00,
+        "image": "mathsPic.jpg",
+        "spaces": 10,
+        "rating": 4
+    },
+
+    {
+
+        "id": 1002,
+        "name": "Science",
+        "location": "Hendon",
+        "price": 35.00,
+        "image": "mathsPic.jpg",
+        "spaces": 8,
+        "rating": 3
+    }
+
+
+]
