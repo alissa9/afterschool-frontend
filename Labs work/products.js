53ff955b8d1c03cbdafe3655ee64e7bffd3d81f8
Ikbal Alissa <m99alissa@gmail.com>
2021-11-15 00:22:03 +0000

    added sort and updated css

--- Labs work/products.js
+++ Labs work/products.js
@@ -1,105 +1,85 @@
 let products = [
-    {
+  {
+    id: 1001,
+    name: "Mathmatics",
+    location: "Hendon",
+    price: 40.0,
+    image: "images/maths.jpg",
+    spaces: 10,
+  },
 
-        "id": 1001,
-        "name": "Mathmatics",
-        "location": "Hendon",
-        "price": 40.00,
-        "image": "images/maths.jpg",
-        "spaces": 10,
-        "rating": 4
-    },
+  {
+    id: 1002,
+    name: "Chemistry",
+    location: "Hendon",
+    price: 35.0,
+    image: "images/chemistry.jpg",
+    spaces: 6,
+  },
 
-    {
+  {
+    id: 1003,
+    name: "Arabic",
+    location: "Hendon",
+    price: 47.0,
+    image: "images/arabic.jpg",
+    spaces: 6,
+  },
 
-        "id": 1002,
-        "name": "Chemistry",
-        "location": "Hendon",
-        "price": 35.00,
-        "image": "images/chemistry.jpg",
-        "spaces": 6,
-        "rating": 3
-    },
-
-
-
-    { 
-
-         "id": 1003,
-         "name": "Arabic",
-         "location": "Hendon",
-         "price": 47.00,
-         "image": "images/arabic.jpg",
-         "spaces": 6,
-         "rating": 3
-    },
-
-    {
-        "id": 1004,
-        "name": "IT",
-        "location": "Hendon",
-        "price": 47.00,
-        "image": "images/IT.jpg",
-        "spaces": 6,
-        "rating": 3
-
-    },
-    {
-        "id": 1005,
-        "name": "English",
-        "location": "Hendon",
-        "price": 47.00,
-        "image": "images/english.jpg",
-        "spaces": 6,
-        "rating": 3
-
-    },
-    {
-        "id": 1006,
-        "name": "Drama",
-        "location": "Hendon",
-        "price": 39.00,
-        "image": "images/drama.jpg",
-        "spaces": 5,
-        "rating": 4
-    },
-    {
-        "id": 1007,
-        "name": "Swimming",
-        "location": "Hendon",
-        "price": 55.00,
-        "image": "images/swimming.jpg",
-        "spaces": 5,
-        "rating": 4
-    },
-    {
-        "id": 1008,
-        "name": "History",
-        "location": "Hendon",
-        "price": 46.00,
-        "image": "images/history.jpg",
-        "spaces": 7,
-        "rating": 4
-
-    },
-    {
-        "id": 1009,
-        "name": "Geography",
-        "location": "Hendon",
-        "price": 46.00,
-        "image": "images/geography.jpg",
-        "spaces": 9,
-        "rating": 4
-
-    },
-    {
-        "id": 1010,
-        "name": "Music",
-        "location": "Hendon",
-        "price": 46.00,
-        "image": "images/music.jpg",
-        "spaces": 8,
-        "rating": 4
-
-    }
-]
+  {
+    id: 1004,
+    name: "IT",
+    location: "Hendon",
+    price: 47.0,
+    image: "images/IT.jpg",
+    spaces: 6,
+  },
+  {
+    id: 1005,
+    name: "English",
+    location: "Hendon",
+    price: 47.0,
+    image: "images/english.jpg",
+    spaces: 6,
+  },
+  {
+    id: 1006,
+    name: "Drama",
+    location: "Hendon",
+    price: 39.0,
+    image: "images/drama.jpg",
+    spaces: 5,
+  },
+  {
+    id: 1007,
+    name: "Swimming",
+    location: "Hendon",
+    price: 55.0,
+    image: "images/swimming.png",
+    spaces: 5,
+  },
+  {
+    id: 1008,
+    name: "History",
+    location: "Hendon",
+    price: 46.0,
+    image: "images/history.jpg",
+    spaces: 7,
+  },
+  {
+    id: 1009,
+    name: "Geography",
+    location: "Hendon",
+    price: 46.0,
+    image: "images/geography.jpg",
+    spaces: 9,
+  },
+  {
+    id: 1010,
+    name: "Music",
+    location: "Hendon",
+    price: 46.0,
+    image: "images/music.jpg",
+    spaces: 8,
+  },
+];
